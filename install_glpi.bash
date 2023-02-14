1d05a0f5fdd3b413080b577a12838b959e9c4466
Jonatxr <47691005+Jonatxr@users.noreply.github.com>
2023-02-14 19:50:25 +0100

    Update install_glpi.bash

--- install_glpi.bash
+++ install_glpi.bash
@@ -12,11 +12,26 @@ GLPI_VHOST_NAME=glpi.sio.local
 apt-get update
 apt-get install -y apache2 mariadb-server php libapache2-mod-php php-mysql php-curl php-fileinfo php-gd php-json php-mbstring php-mysqli php-session php-zlib php-simplexml php-xml php-intl
 
+# Delete GLPI if already exists
+if [ -d "/var/www/html/glpi" ]; then
+    rm -rf /var/www/html/glpi
+fi
+if [ -d "/var/www/glpi" ]; then
+    rm -rf /var/www/glpi
+fi
 # Download GLPI
 wget https://github.com/glpi-project/glpi/releases/download/$GLPI_VERSION/glpi-$GLPI_VERSION.tgz
 tar xvfz glpi-$GLPI_VERSION.tgz
 mv glpi /var/www/
 chown -R www-data:www-data /var/www/glpi
+
+# Delete existing database and user if they exist
+if mysql -u root -p$MYSQL_ROOT_PASSWORD -e "use $GLPI_DB_NAME"; then
+    mysql -u root -p$MYSQL_ROOT_PASSWORD -e "DROP DATABASE $GLPI_DB_NAME;"
+fi
+if mysql -u root -p$MYSQL_ROOT_PASSWORD -e "SELECT User FROM mysql.user WHERE User='$GLPI_DB_USER'" | grep $GLPI_DB_USER; then
+    mysql -u root -p$MYSQL_ROOT_PASSWORD -e "DROP USER $GLPI_DB_USER@localhost;"
+fi
 
 # Create GLPI database and user
 mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE $GLPI_DB_NAME;"
