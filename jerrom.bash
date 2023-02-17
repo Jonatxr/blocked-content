2f779ed875fd9f68cd504fa65c6a2ceee58371bd
Jonatxr <47691005+Jonatxr@users.noreply.github.com>
2023-02-17 12:31:08 +0100

    Update jerrom.bash

--- jerrom.bash
+++ jerrom.bash
@@ -13,10 +13,10 @@ sudo add-apt-repository ppa:ondrej/php7.4
 
 # Install required packages and php7.4 extensions
 sudo apt update
-sudo apt install -y php7.4 php7.4-fpm php7.4-curl php7.4-fileinfo php7.4-gd php7.4-mbstring php7.4-mysqli php7.4-intl php7.4-zlib php7.4-simplexml php7.4-xml php7.4-json libapache2-mod-php7.4
-sudo apt install php7.4-curl
-sudo apt install apache2
-sudo apt install mysql-server
+sudo apt -y install apache2
+sudo apt -y install mysql-server
+sudo apt -y install php7.4
+sudo apt-get install -y php7.4-cli php7.4-json php7.4-common php7.4-mysql php7.4-zip php7.4-gd php7.4-mbstring php7.4-curl php7.4-xml php7.4-bcmath
 
 #Create working repo
 mkdir /tempglpi
